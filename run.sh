9f39b51d32c69b977f03a5ea94525430df86f757
liangzi <liangzi@staff.sina.com.cn>
2021-06-09 19:14:49 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 train.py
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1   --master_port 10086 --master_addr 127.0.0.8 train.py
